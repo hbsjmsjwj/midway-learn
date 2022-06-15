cfe1e24ca7648fff6b5e0863f1bd348a0c987d4a
蒋文杰 <zt22243@ztn.com>
2022-06-15 11:42:39 +0800

    添加swagger

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,6 +3,7 @@ import { ILifeCycle } from '@midwayjs/core';
 import { Application } from 'egg';
 import { join } from 'path';
 import * as orm from '@midwayjs/orm';
+import * as swagger from '@midwayjs/swagger';
 import * as egg from '@midwayjs/web';
 
 @Configuration({
@@ -18,9 +19,18 @@ export class ContainerLifeCycle implements ILifeCycle {
 
 @Configuration({
   imports: [
-    // ...
     orm, // 加载 orm 组件
   ],
   importConfigs: [join(__dirname, './config')],
 })
 export class ContainerConfiguratin {}
+
+@Configuration({
+  imports: [
+    {
+      component: swagger,
+      enabledEnvironment: ['local'],
+    },
+  ],
+})
+export class ContainerConfiguration {}
